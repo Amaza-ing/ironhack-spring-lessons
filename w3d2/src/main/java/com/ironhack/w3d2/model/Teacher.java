238ae0364d7a6a1f618a71277e86bd6926f1ff0a
Adrián Maza <adri_mv1@hotmail.com>
2022-11-22 10:55:59 +0100

    spring lessons created

---
+++ w3d2/src/main/java/com/ironhack/w3d2/model/Teacher.java
@@ -0,0 +1,42 @@
+package com.ironhack.w3d2.model;
+
+import javax.persistence.*;
+
+@Entity // imprescindible en todas las entidades
+@Table(name = "teacher") // necesario cuando el nombre de la tabla sea diferente al de la clase
+public class Teacher {
+    @Id // annotation para indicar primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // annotation para indicar auto_increment
+    @Column(name = "id") // necesario si el campo de SQL tiene otro nombre
+    private Integer id;
+
+//    @Column(name = "teacher")
+    private String teacher;
+
+//  Necesitamos el constructor vacío
+    public Teacher() {
+    }
+
+//  Necesitamos el constructor con todos los elementos
+
+    public Teacher(Integer id, String teacher) {
+        this.id = id;
+        this.teacher = teacher;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+}
