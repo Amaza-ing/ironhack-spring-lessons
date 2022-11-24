78ab4dac5e3fbe700d95104373f8cd24d4c1c83a
Adrián Maza <adri_mv1@hotmail.com>
2022-11-24 09:18:30 +0100

    w3d4 relationships lesson created

---
+++ w3d4/src/main/java/com/ironhack/w3d4/model/MultipleChoiceExam.java
@@ -0,0 +1,27 @@
+package com.ironhack.w3d4.model;
+
+import javax.persistence.Entity;
+import javax.persistence.PrimaryKeyJoinColumn;
+import java.util.Date;
+
+@Entity
+@PrimaryKeyJoinColumn(name = "course")
+public class MultipleChoiceExam extends Exam{
+    private Integer numberOfChoices;
+
+    public MultipleChoiceExam() {
+    }
+
+    public MultipleChoiceExam(String course, Date startDate, Boolean mandatory, Integer numberOfChoices) {
+        super(course, startDate, mandatory);
+        this.numberOfChoices = numberOfChoices;
+    }
+
+    public Integer getNumberOfChoices() {
+        return numberOfChoices;
+    }
+
+    public void setNumberOfChoices(Integer numberOfChoices) {
+        this.numberOfChoices = numberOfChoices;
+    }
+}
