724b06334944dab4f948deb98fec1616601b224a
Adrián Maza <adri_mv1@hotmail.com>
2022-11-28 08:57:55 +0100

    w4d1 created

---
+++ w4d1/src/main/java/com/ironhack/w4d1/W4d1Application.java
@@ -0,0 +1,25 @@
+package com.ironhack.w4d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W4d1Application {
+	public static void main(String[] args) {
+		SpringApplication.run(W4d1Application.class, args);
+	}
+
+
+
+
+
+
+
+//	public static void main(String[] args) {
+//		ApplicationContext applicationContext = SpringApplication.run(W3d5Application.class, args);
+//		MyClass myClass = applicationContext.getBean(MyClass.class);
+//
+//		myClass.findAll();
+//	}
+
+}
---
+++ w4d1/src/main/java/com/ironhack/w4d1/model/Exam.java
@@ -0,0 +1,49 @@
+package com.ironhack.w4d1.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import java.util.Date;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Exam {
+    @Id
+    private String course;
+    private Date startDate;
+    private Boolean mandatory;
+
+    public Exam() {
+    }
+
+    public Exam(String course, Date startDate, Boolean mandatory) {
+        this.course = course;
+        this.startDate = startDate;
+        this.mandatory = mandatory;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Boolean getMandatory() {
+        return mandatory;
+    }
+
+    public void setMandatory(Boolean mandatory) {
+        this.mandatory = mandatory;
+    }
+}
